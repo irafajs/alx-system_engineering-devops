16d1d2437a68a4fba7d32385730b2e44610a0890
irafajs <irafajs@gmail.com>
2023-11-29 11:56:17 +0200

    TASK 07

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,6 @@
-#install configure nginx using puppet
+#isntall and configue nginx using puppet
 
-exec { 'check-update-linux':
-  command => 'sudo apt-get -y update',
-  }
-
-exec { 'install-nginx':
-  command => 'sudo apt-get install -y nginx',
-  require => Exec['check-update-linux']
-  }
-
-exec { 'create-index-html':
-  command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  require => Exec['install-nginx']
-  }
-
-exec { 'redirectioning':
-  command => 'sed -i "40i location /redirect_me {\nreturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n}\n" /etc/nginx/sites-available/default',
-  require => Exec['install-nginx'],
-  }
-
-exec { 'restart-nginx':
-  command => 'service nginx restart',
-  require => Exec['redirectioning']
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get install -y nginx ; echo "Hello World!" | sudo tee /var/www/html/index.html ; sudo sed -i "40i location /redirect_me {\nreturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n}\n" /etc/nginx/sites-available/default ;  sudo service nginx restart'
   }
