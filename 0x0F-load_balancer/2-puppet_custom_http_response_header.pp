0d8b4e1b25df6e15d401da781703d0565f4936d3
irafajs <irafajs@gmail.com>
2023-12-04 21:12:01 +0200

    TASK02 FINAL

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,28 @@
 #insert customer header in the response
 
 exec {'update':
-  provider => shell,
-  command  => 'sudo apt -y update',
-  before   => Exec['nginx installation'],
+  provider  => shell,
+  command   => 'sudo apt -y update',
+  logoutput => true,
+  before    => Exec['nginx installation']
   }
 
 exec { 'nginx installation':
-  provider => shell,
-  command  => 'sudo apt -y install nginx',
+  provider  => shell,
+  command   => 'sudo apt -y install nginx',
+  logoutput => true
   }
 
 exec { 'custom_header':
   provider    => shell,
   environment => ["HOSTNAME=${hostname}"],
   command     => 'sudo sed -i "29i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
-  before      => Exec['start_nginx'],
+  logoutput   => true,
+  before      => Exec['start_nginx']
   }
 
 exec { 'start_nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+  provider  => shell,
+  command   => 'sudo service nginx restart',
+  logoutput => true
   }
