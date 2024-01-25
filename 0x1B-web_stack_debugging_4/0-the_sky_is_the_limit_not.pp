5a10b3ee2c7ece92f31bef0bac9f565820d8fbe3
irafajs <irafajs@gmail.com>
2024-01-25 19:35:14 +0200

    TASK00

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# increase ulimit number
+exec { 'fix-ulimit':
+  command => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
+  path    => ['/bin', '/usr/bin'],
+}
+
+# nginx restart
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d',
+  refreshonly => true,
+  subscribe   => Exec['fix-ulimit'],
+}
